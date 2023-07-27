e1810c15df6730d94470a595f5c0ef88f3533cc2
Ishita Pathak <75848598+IshitaPathak@users.noreply.github.com>
2023-07-28 00:57:04 +0530

    Added solution - LeetHub

--- Pattern 7 - GFG/pattern-7.cpp
+++ Pattern 7 - GFG/pattern-7.cpp
@@ -8,18 +8,17 @@ class Solution {
   public:
     void printTriangle(int n) {
         // code here
-        //space
-        for(int i=1;i<=n;i++){
-            for(int s=n-1;s>i-1;s--){
-                cout<<" ";
-            }
-            //star
-            for(int j=1;j<=(2*i)-1;j++){
-                cout<<"*";
-            }
-            cout<<endl;
-        }
-    }
+      for(int i=0;i<n;i++){
+          for(int s=n-i-1;s>0;s--){
+              cout<<" ";
+          }
+          for(int j=0;j<2*i+1;j++){
+              cout<<"*";
+          }
+          cout<<endl;
+      }
+          
+      }
 };
 
 //{ Driver Code Starts.
