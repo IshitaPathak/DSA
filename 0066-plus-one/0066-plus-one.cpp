4fafa7b7a38d397917ae4450a266ee2f094bd1ee
Ishita Pathak <75848598+IshitaPathak@users.noreply.github.com>
2023-10-24 23:50:57 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (19.14%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -11,7 +11,7 @@ public:
             }
         }
         // if the loop completes withouth returning inside, it means all the element were 9.
-        // In this case , it adds a new element 0 to the end of the digits vector and set the first element to         1( as a carry over)
+        // In this case , it adds a new element 0 to the end of the digits vector and set the first element to 1( as a carry over)
         digits.insert(digits.begin(),1);
         return digits;
     }
