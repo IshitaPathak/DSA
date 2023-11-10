c987465c8733a7ee8584c15d5a28fadf2a8f060a
Ishita Pathak <75848598+IshitaPathak@users.noreply.github.com>
2023-11-11 02:43:58 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (94.41%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
+        //base case/edge case
         if(n<=1) return n;
-        return fib(n-1)+fib(n-2);
+        
+        int map[n+1];
+        map[0]=0;
+        map[1]=1;
+        for(int i=2;i<=n;i++){
+            map[i]=map[i-1]+map[i-2];
+        }
+        return map[n];
     }
 };
