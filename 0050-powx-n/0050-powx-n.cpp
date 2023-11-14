e5c2f4ec15ee99c1adc91081d7235e2e6119dc77
Ishita Pathak <75848598+IshitaPathak@users.noreply.github.com>
2023-11-14 18:37:52 +0530

    Time: 3 ms (41.89%), Space: 6.3 MB (94.34%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:    
     double myPow(double x, int n) {
+//         int res=1;
+//         while(n>0){
+//              if(n%2!=0){
+//             res=res*x;
+//             }
+//             x=x*x;
+//             n=n/2;
+//         }
+        
+//         return res;
+       
     return solve(x, n, 1);
 }
 
 double solve(double x, int n, double res){
     if (n == 0) return res;
     
-    if (n%2 != 0){
-        if (n>0) res *= x;
-        else res /= x;
+    // if (n%2 != 0){
+    if (n&1 !=0){
+        if (n>0) res = res*x;
+        else res = res/x;
     }
     
     return solve(x*x, n/2, res);
-}
+  }
 };
