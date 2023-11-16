da81427b79f416e7b8065abbbec7e50fcecc0a85
Ishita Pathak <75848598+IshitaPathak@users.noreply.github.com>
2023-11-17 02:41:10 +0530

    Time: 0 ms (100.00%), Space: 10.4 MB (86.71%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-        // convert every binary number to integer and add it to set
-        // now iterate through 0 to 2 to the power n-1 when n is length of string
-        // if its not there in the set its the answer
-        unordered_set<int> s;
         int n=nums.size();
+        string result="";
         for(int i=0;i<n;i++){
-            s.insert(stoi(nums[i],0,2));
+            if(nums[i][i]=='0'){
+                result+='1';
+            }else{
+                result+='0';
+            }
         }
-        // int m=pow(2,n);
-        for(int i=0;i<n+1;i++){
-             if (s.find(i) == s.end()) {
-            // If not in the set, it's the answer
-            string binaryStr = bitset<64>(i).to_string().substr(64 - n);
-            return binaryStr;
-          }
-        }
-        return "";
+        return result;
     }
 };
