ada6e6086e88673d5e559109e8b1a3c3627df767
Ishita Pathak <75848598+IshitaPathak@users.noreply.github.com>
2023-11-17 22:40:17 +0530

    Time: 181 ms (76.25%), Space: 96.6 MB (61.58%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -5,7 +5,7 @@ public:
         int maxi =INT_MIN;
         int sum=0;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n/2;i++){
            sum= nums[i]+nums[n-i-1];
             maxi=max(sum,maxi);
         }
