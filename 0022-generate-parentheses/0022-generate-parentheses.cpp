7fa5e7cc7f742fdb82c85c76be14b0eba7a5d8ba
Ishita Pathak <75848598+IshitaPathak@users.noreply.github.com>
2023-11-19 00:50:54 +0530

    Time: 6 ms (26.60%), Space: 16.5 MB (26.23%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -7,7 +7,7 @@ public:
         }
         if(open!=0){
             string op1 = op;
-            op1.push_back('(');
+            op1.push_back('('); // string ko char array ki tharah treat karta hai to push back character hoga and you can't use " " double quote bcz that's for string 
             solve(open-1,close,op1,v);
         }
         if(close!=0 && close>open){
