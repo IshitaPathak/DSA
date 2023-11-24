e5dddab4c159027b46ae922b5809d8070222f337
Ishita Pathak <75848598+IshitaPathak@users.noreply.github.com>
2023-11-25 00:09:22 +0530

    Time: 82 ms (82.43%), Space: 53.9 MB (72.56%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
     
-   vector<vector<string>> partition(string s) {
-        vector<vector<string> > ret;
-        if(s.empty()) return ret;
-        
-        vector<string> path;
-        dfs(0, s, path, ret);
-        
-        return ret;
+    bool isPalindrome(string s,int start,int end){
+         while(start<=end){
+               if(s[start++]!=s[end--]) return false;
+          }
+        return true;
     }
-    
-    void dfs(int index, string& s, vector<string>& path, vector<vector<string> >& ret) {
-        if(index == s.size()) {
-            ret.push_back(path);
+    void fun(int ind,string &s,vector<string> &path,vector<vector<string>> &ans){
+        if(ind==s.size()){
+           ans.push_back(path);
             return;
         }
-        for(int i = index; i < s.size(); ++i) {
-            if(isPalindrome(s, index, i)) {
-                path.push_back(s.substr(index, i - index + 1));
-                dfs(i+1, s, path, ret);
+        for(int i=ind;i<s.size();i++){
+           if(isPalindrome(s,ind,i)){
+                path.push_back(s.substr(ind,i-ind+1));
+                fun(i+1,s,path,ans);
                 path.pop_back();
             }
         }
-    }
+    }   
     
-    bool isPalindrome(const string& s, int start, int end) {
-        while(start <= end) {
-            if(s[start++] != s[end--])
-                return false;
-        }
-        return true;
+    
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> path;
+        // fun(ind,string,path,result);
+        fun(0,s,path,ans);
+        return ans;
     }
 };
