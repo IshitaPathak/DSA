c9c9d3c6ab35f7be7523278edf980de483f6ff1f
Ishita Pathak <75848598+IshitaPathak@users.noreply.github.com>
2023-12-01 05:33:45 +0530

    Time: 4 ms (31.66%), Space: 6.3 MB (58.09%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        // TC - log(n)
-        int count=0;
-        while(n!=0){
-           count+= n%2;      
-           n=n/2; 
-
-        }
-        return count;
+        return  __builtin_popcount(n);
     }
 };
