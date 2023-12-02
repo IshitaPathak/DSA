bbebd60163ba2d042c98fe093e8d26dfb57351e4
Ishita Pathak <75848598+IshitaPathak@users.noreply.github.com>
2023-12-03 00:20:24 +0530

    Time: 77 ms (51.00%), Space: 27.2 MB (47.63%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     int countCharacters(vector<string>& words, string chars) {
-        vector<int> charCount(26,0);
-        for(int i=0;i<chars.size();i++){
-            charCount[(chars[i])-'a']++;
+        unordered_map<char,int> m;
+        int result=0;
+       // Count the occurrences of each character in chars
+        for (int i = 0; i < chars.size(); i++) {
+            m[chars[i]]++;
         }
-          int result=0;
-          
-        for(int j=0;j<words.size();j++){
-             vector<int> wordCount(26,0);
-            for(int k=0;k<words[j].size();k++){
-                wordCount[(words[j][k])-'a']++;
-            }
-            bool good =true;
-            for(int i=0;i<26;i++){
-                if(wordCount[i]>charCount[i]){
-                     good =false;
+        // Check each word in the array
+        for (int j = 0; j < words.size(); j++) {
+             unordered_map<char,int> s;
+            bool goodWord = true;
+            // Count the occurrences of each character in the current word
+            for (int k = 0; k < words[j].size(); k++) {
+                char c = words[j][k];
+                s[c]++;
+                // Check if the character count exceeds the available count in chars
+                if (s[c] > m[c]) {
+                    goodWord = false;
                     break;
-                 }
+                }
             }
-            
-            if(good==true){
-                result+=words[j].size();
+            // If the word is "good," add its length to the result
+            if (goodWord) {
+                result += words[j].size();
             }
-        }                      
-          return result;            
+        }
+        return result;
     }
 };
