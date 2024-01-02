a51cd0e93a772c5f59e9a9deddd271409f482850
Ishita Pathak <75848598+IshitaPathak@users.noreply.github.com>
2024-01-03 01:13:56 +0530

    Time: 45 ms (36.63%), Space: 17.9 MB (37.08%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int i=0;
-        int j=0;
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
-        while(i<g.size() && j<s.size()){
-                    if(s[j]>=g[i]){
-                        i++;
-                    }
-                j++;
-            }
-        return i;
+        int n=g.size(); int m=s.size();
+        int cnt=0;
+        int i=0;
+        int j=0;
+        while(i<n && j<m){
+           
+                if(g[i]<=s[j]){
+                    cnt++;
+                  i++;
+                }
+                   j++;
+            
+            
+                           
+
         }
+        return cnt;
+    }
 };
